cf49c5cf8781da497b81df69680c86bf9c10da13
Hernan Y.Ke <yuriqiao@gmail.com>
2016-04-30 16:44:58 -0400

    L03 array with index not working

--- l03_defaulthashval.rb
+++ l03_defaulthashval.rb
@@ -5,8 +5,14 @@ sentence= "single web app on web"
 
 result =Hash.new(0)
 sentence.split(" ").each do |word|
-  result[word]||=0
   result[word]+=1
 end
 
 p result
+
+res = Hash.new([])
+sentence.split(" ").each_with_index do |word,index|
+  result[word]<<index
+end
+
+p res
