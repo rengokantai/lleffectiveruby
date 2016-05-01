4e56c5e0cdafeec596d8bf3a69513389ed1d89a5
Hernan Y.Ke <yuriqiao@gmail.com>
2016-04-30 22:42:22 -0400

    changed

--- l10_prefermethodoverconst.rb
+++ l10_prefermethodoverconst.rb
@@ -1,11 +1,14 @@
 class L10_prefermethodoverconst
   CON = [1,2]
   def con
-    CON.select!{|x|x%2==0}
+    cons.select!{|x|x%2==0}
+  end
+  def cons
+    [1,2]
   end
 end
 
 c = L10_prefermethodoverconst.new
 p L10_prefermethodoverconst::CON
 p c.con
-p L10_prefermethodoverconst::CON  #Not good.
+p L10_prefermethodoverconst::CON  #good.
